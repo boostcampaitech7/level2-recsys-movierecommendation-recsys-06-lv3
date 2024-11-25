472db554f4a04f9dab928d9ecbcf45557c527639
유대선_T7537 <xenx96@naver.com>
2024-11-25 17:22:55 +0900

    feat: sasrecf recbole 모델 탑재

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 from omegaconf import OmegaConf
 
 import src.data as data_module
+from src.data.DataInterface import DataInterface
 from src.model import Model
 
 
 def app():
     cfg = OmegaConf.load("./config/config.yaml")
+    model_type: str = cfg["model"]["type"]
+    recbole_cfg_path = f"./config/model_configs/{model_type.lower()}.yaml"
+    model_cfg = OmegaConf.load(recbole_cfg_path)
     ## Data Pre-Processing & Export data(for recbole)
     print("---------Start Data Pre-Processing---------")
     data_type: str = cfg["data"]["type"]
-    data_path = cfg["data"]["path"]
-    data_class = getattr(data_module, data_type)(data_path)
+    data_path = cfg["data"]["base_path"]
+    _: DataInterface = getattr(data_module, data_type)(model_cfg["dataset"], data_path, model_cfg["data_path"])
 
     ## (Train or Train/Valid) & Inference
     mode = cfg["model"]["mode"]
     print(f"-------Start Train{mode}------------")
-    model_type: str = cfg["model"]["type"]
-    recbole_cfg_path = f"./config/model_configs/{model_type.lower()}.yaml"
     model_save_path = cfg["model"]["save_path"]
     model = Model(recbole_cfg_path, model_save_path, mode)
     return
--- src/data/DataInterface.py
+++ src/data/DataInterface.py
@@ -5,8 +5,12 @@ import pandas as pd
 
 
 class DataInterface(ABC):
-    def __init__(self, data_path: str):
+    def __init__(self, dataset, data_path: str, export_path: str):
+        assert dataset is not None or dataset != ""
+        self.dataset = dataset
         self.data_path = data_path
+        self.export_path = export_path
+        self.export_dfs: dict[str, pd.DataFrame] = {}
         self.data: dict[str, pd.DataFrame] = {}
         self._load_data()
         self.pre_processing()
@@ -21,10 +25,12 @@ class DataInterface(ABC):
         filelist = os.listdir(self.data_path)
         for file in filelist:
             if file.endswith(".csv"):
+                print("Load File:" + file)
                 df = pd.read_csv(os.path.join(self.data_path, file), sep=",")
                 self.data.update({file.replace(".csv", ""): df})
             elif file.endswith(".tsv"):
-                df = pd.read_csv(os.path.join(self.data_path, file), sep=",")
+                print("Load File:" + file)
+                df = pd.read_csv(os.path.join(self.data_path, file), sep="\t")
                 self.data.update({file.replace(".tsv", ""): df})
 
     @abstractmethod
